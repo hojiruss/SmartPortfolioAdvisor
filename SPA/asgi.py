664a52dd3c6f7bebb371304eb185e1c25a7c65f8
HojiRuss <hojat96shoapour@gmail.com>
2026-09-11 00:41:31 +0330

    change repository and project name

--- SPA/asgi.py
+++ SPA/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EcoSanaz project.
+ASGI config for SmartPortfolioAdvisor project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EcoSanaz.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SmartPortfolioAdvisor.settings')
 
 application = get_asgi_application()
